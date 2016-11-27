a07ce8e962b2525f495413ed7dcbe0daa810aba6
Andrew Johnson <drewjohnson.j@gmail.com>
2016-11-27 12:15:51 -0600

    update settings
    Signed-off-by: Andrew Johnson <drewjohnson.j@gmail.com>

--- ajwad/ajwad/settings.py
+++ ajwad/ajwad/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!tno3-n8w3z(lli&5=!pc_$ws4jnu8o17$aa#=1fme)0o_%6m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,4 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
